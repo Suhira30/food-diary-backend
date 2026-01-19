6a71cc860e90ae161fda0c07d0230b4b92fc93c5
Suhira30 <suhirabalarajan@gmail.com>
2026-01-19 16:39:15 +0530

    feat: initial backend setup

--- src/main/java/org/example/foodtrack/FoodTrackApplication.java
+++ src/main/java/org/example/foodtrack/FoodTrackApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodTrackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodTrackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoodTrackApplication.class, args);
+    }
 
 }
