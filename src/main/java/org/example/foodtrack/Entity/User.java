6e6eff67a9119fa483da74af73aeb912a18bf732
Suhira30 <suhirabalarajan@gmail.com>
2026-01-20 23:32:31 +0530

    feat: Entity User created

---
+++ src/main/java/org/example/foodtrack/Entity/User.java
@@ -0,0 +1,25 @@
+package org.example.foodtrack.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
