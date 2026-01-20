d76457b721de3063c468d178cfbc158e8bbcc751
Suhira30 <suhirabalarajan@gmail.com>
2026-01-20 23:31:39 +0530

    feat: FoodDiaryResponse response DTO created

---
+++ src/main/java/org/example/foodtrack/Dto/Response/FoodDiaryResponse.java
@@ -0,0 +1,11 @@
+package org.example.foodtrack.Dto.Response;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodDiaryResponse {
+    private String message ;
+    private int statusCode;
+}
