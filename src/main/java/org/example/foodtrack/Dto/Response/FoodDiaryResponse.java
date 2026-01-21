1409cb1c761f927f07a44b091a3fdb876e067a34
Suhira30 <suhirabalarajan@gmail.com>
2026-01-21 20:32:20 +0530

    feat: constructors added to FoodDiaryResponse.java

--- src/main/java/org/example/foodtrack/Dto/Response/FoodDiaryResponse.java
+++ src/main/java/org/example/foodtrack/Dto/Response/FoodDiaryResponse.java
@@ -1,11 +1,27 @@
 package org.example.foodtrack.Dto.Response;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class FoodDiaryResponse {
-    private String message ;
+
+    private Object data;
+    private String message;
     private int statusCode;
+
+    public FoodDiaryResponse(String token, String message, int statusCode) {
+        this.data = token;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    public FoodDiaryResponse(String message, int statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
 }
