58d8a3060d7ebc665a5a56e9ddbc99b47097bd5b
Suhira30 <suhirabalarajan@gmail.com>
2026-01-21 20:32:00 +0530

    feat: user login

--- src/main/java/org/example/foodtrack/Controller/AuthController.java
+++ src/main/java/org/example/foodtrack/Controller/AuthController.java
@@ -2,13 +2,11 @@ package org.example.foodtrack.Controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.foodtrack.Dto.Request.LoginRequest;
-import org.example.foodtrack.Dto.Response.FoodDiaryResponse;
 import org.example.foodtrack.Dto.Request.RegisterRequest;
+import org.example.foodtrack.Dto.Response.FoodDiaryResponse;
 import org.example.foodtrack.Service.UserImpl;
-import org.example.foodtrack.Util.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,8 +15,6 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserImpl userImpl;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtUtil jwtUtil;
 
     @GetMapping("/own_test")
     public String test() {
@@ -28,13 +24,14 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<FoodDiaryResponse> register(@RequestBody RegisterRequest request) {
-        FoodDiaryResponse foodDiaryResponse=userImpl.userRegister(request);
+        FoodDiaryResponse foodDiaryResponse = userImpl.userRegister(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(foodDiaryResponse);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok("token");
+    public ResponseEntity<FoodDiaryResponse> login(@RequestBody LoginRequest request) {
+        FoodDiaryResponse foodDiaryResponse = userImpl.userLogin(request);
+        return ResponseEntity.status(HttpStatus.OK).body(foodDiaryResponse);
     }
 
 }
--- src/main/java/org/example/foodtrack/Service/UserImpl.java
+++ src/main/java/org/example/foodtrack/Service/UserImpl.java
@@ -1,15 +1,20 @@
 package org.example.foodtrack.Service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.foodtrack.Dto.Request.LoginRequest;
 import org.example.foodtrack.Dto.Request.RegisterRequest;
 import org.example.foodtrack.Dto.Response.FoodDiaryResponse;
 import org.example.foodtrack.Entity.User;
 import org.example.foodtrack.Exception.BadRequestException;
 import org.example.foodtrack.Exception.ConflictException;
+import org.example.foodtrack.Exception.NotFoundException;
 import org.example.foodtrack.Repo.UserRepository;
+import org.example.foodtrack.Util.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,28 +22,51 @@ public class UserImpl {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public FoodDiaryResponse userRegister(RegisterRequest registerRequest) {
+        try {
+            if(registerRequest.getEmail() == null || registerRequest.getEmail().isBlank()) {
+                throw new BadRequestException("Email is required");
+            }
 
-        if (registerRequest.getEmail() == null || registerRequest.getEmail().isBlank()) {
-            throw new BadRequestException("Email is required");
+            if (userRepository.findByEmail(registerRequest.getEmail()).isPresent()) {
+                throw new ConflictException("Email already exists");
+            }
+
+            User user = new User();
+            user.setName(registerRequest.getName());
+            user.setEmail(registerRequest.getEmail());
+            user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+
+            userRepository.save(user);
+
+            return new FoodDiaryResponse(
+                    "User registered successfully",
+                    HttpStatus.CREATED.value()
+            );
+        } catch (Exception ex) {
+            throw new BadRequestException("Error in userRegister Impl"+ex.getMessage());
         }
 
-        if (userRepository.findByEmail(registerRequest.getEmail()).isPresent()) {
-            throw new ConflictException("Email already exists");
+    }
+
+    public FoodDiaryResponse userLogin(LoginRequest loginRequest) {
+        try {
+            Optional<User> user = userRepository.findByEmail(loginRequest.getEmail());
+            if (user.isEmpty()) {
+                throw new NotFoundException("User not Founded");
+            }
+
+            if (!passwordEncoder.matches(loginRequest.getPassword(), user.get().getPassword())) {
+                throw new BadRequestException("Wrong Password");
+            }
+
+            String token = jwtUtil.generateToken(user.get().getEmail());
+            return new FoodDiaryResponse(token,"User Login Successfully",HttpStatus.OK.value());
+        } catch (Exception ex) {
+            throw new BadRequestException("Error in UserLongin Impl"+ex.getMessage());
         }
-
-        User user = new User();
-        user.setName(registerRequest.getName());
-        user.setEmail(registerRequest.getEmail());
-        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
-
-        userRepository.save(user);
-
-        return new FoodDiaryResponse(
-                "User registered successfully",
-                HttpStatus.CREATED.value()
-        );
 
     }
 }
