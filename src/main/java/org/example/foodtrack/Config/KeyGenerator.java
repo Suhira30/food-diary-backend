def8eed986526f9bf7656036e1f93d898b121440
Suhira30 <suhirabalarajan@gmail.com>
2026-01-21 22:24:57 +0530

    feat: configuration

---
+++ src/main/java/org/example/foodtrack/Config/KeyGenerator.java
@@ -0,0 +1,16 @@
+package org.example.foodtrack.Config;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class KeyGenerator {
+    public static void main(String[] args) {
+        // Generate a secure key
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
+        System.out.println("Secure JWT Secret Key: " + base64Key);
+    }
+}
