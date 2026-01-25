0ef4d5f7c49b656ed06f0c4a5dbbd8855d753202
Suhira30 <suhirabalarajan@gmail.com>
2026-01-25 23:44:09 +0530

    Feat: Constructor based object creation in RestaurantResponse.java

--- src/main/java/org/example/foodtrack/Dto/Response/RestaurantResponse.java
+++ src/main/java/org/example/foodtrack/Dto/Response/RestaurantResponse.java
@@ -1,11 +1,17 @@
 package org.example.foodtrack.Dto.Response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.foodtrack.Entity.Restaurant;
+
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class RestaurantResponse {
     private Long id;
     private String name;
@@ -19,4 +25,17 @@ public class RestaurantResponse {
     private Integer totalReviews;
     private LocalDateTime createdAt;
 
+    public RestaurantResponse(Restaurant restaurant) {
+        this.id=restaurant.getId();
+        this.name=restaurant.getName();
+        this.description=restaurant.getDescription();
+        this.location=restaurant.getLocation();
+        this.cuisine=restaurant.getCuisine();
+        this.imageUrl=restaurant.getImageUrl();
+        this.createdByName=restaurant.getCreatedByName();
+        this.createdById=restaurant.getId();
+        this.averageRating=restaurant.getAverageRating();
+        this.totalReviews=restaurant.getTotalReviews();
+        this.createdAt=restaurant.getCreatedAt();
+    }
 }
