43549b859f80303a47e8ead619829b33ff2da50b
Suhira30 <suhirabalarajan@gmail.com>
2026-01-25 23:47:02 +0530

    Chore: modification in DuplicateRestaurantResponse.java

--- src/main/java/org/example/foodtrack/Dto/Response/DuplicateRestaurantResponse.java
+++ src/main/java/org/example/foodtrack/Dto/Response/DuplicateRestaurantResponse.java
@@ -1,10 +1,16 @@
 package org.example.foodtrack.Dto.Response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class DuplicateRestaurantResponse {
     private boolean hasDuplicates;
     private String message;
