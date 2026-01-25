5321fe95b244d71cf49da1fab94f0da7e2558e1a
Suhira30 <suhirabalarajan@gmail.com>
2026-01-25 23:44:29 +0530

    Chore:modification in RestaurantController.java

--- src/main/java/org/example/foodtrack/Controller/RestaurantController.java
+++ src/main/java/org/example/foodtrack/Controller/RestaurantController.java
@@ -7,18 +7,19 @@ import org.example.foodtrack.Dto.Response.RestaurantResponse;
 import org.example.foodtrack.Service.RestaurantImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/restaurant")
+@RequestMapping("/v1/food-diary")
 public class RestaurantController {
 
     private final RestaurantImpl restaurantImpl;
 
-    @RequestMapping("/add/retaurant")
+    @PostMapping("/add/restaurant")
     public ResponseEntity<RestaurantResponse> createRestaurant(CreateRestaurantReq createRestaurantReq, Authentication authentication, @RequestParam(defaultValue = "false") boolean force) {
         String email = authentication.name();
         RestaurantResponse restaurantResponse = restaurantImpl.createRestaurant(createRestaurantReq, email,force);
