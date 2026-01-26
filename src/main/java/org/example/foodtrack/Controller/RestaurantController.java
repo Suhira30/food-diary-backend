eea4ce9fea6fa8358a6864c00663059dae29187a
Suhira30 <suhirabalarajan@gmail.com>
2026-01-26 23:43:30 +0530

    chore: Controller class modified

--- src/main/java/org/example/foodtrack/Controller/RestaurantController.java
+++ src/main/java/org/example/foodtrack/Controller/RestaurantController.java
@@ -1,16 +1,13 @@
 package org.example.foodtrack.Controller;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.core.Authentication;
 import org.example.foodtrack.Dto.Request.CreateRestaurantReq;
 import org.example.foodtrack.Dto.Response.RestaurantResponse;
 import org.example.foodtrack.Service.RestaurantImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,8 +17,10 @@ public class RestaurantController {
     private final RestaurantImpl restaurantImpl;
 
     @PostMapping("/add/restaurant")
-    public ResponseEntity<RestaurantResponse> createRestaurant(CreateRestaurantReq createRestaurantReq, Authentication authentication, @RequestParam(defaultValue = "false") boolean force) {
-        String email = authentication.name();
+    public ResponseEntity<RestaurantResponse> createRestaurant(@RequestBody CreateRestaurantReq createRestaurantReq,
+                                                               @RequestParam(defaultValue = "false") boolean force,
+                                                               Authentication authentication) {
+        String email = authentication.getName();
         RestaurantResponse restaurantResponse = restaurantImpl.createRestaurant(createRestaurantReq, email,force);
         return ResponseEntity.status(HttpStatus.CREATED).body(restaurantResponse);
     }
