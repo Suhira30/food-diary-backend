f2aecebecc5b50f847cbbe5165d4ae23d1040882
Suhira30 <suhirabalarajan@gmail.com>
2026-01-30 21:33:08 +0530

    feat: api for update dairy  /v1/food-diary/diary/update/{id}

--- src/main/java/org/example/foodtrack/Controller/DairyController.java
+++ src/main/java/org/example/foodtrack/Controller/DairyController.java
@@ -2,15 +2,14 @@ package org.example.foodtrack.Controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.foodtrack.Dto.Request.CreateDiaryEntryRequest;
+import org.example.foodtrack.Dto.Request.UpdateDiaryEntryRequest;
 import org.example.foodtrack.Dto.Response.DiaryEntryResponse;
 import org.example.foodtrack.Service.DiaryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,5 +26,13 @@ public class DairyController {
         DiaryEntryResponse diaryEntryResponse = diaryService.addDiaryEntry(request, email);
         return ResponseEntity.status(HttpStatus.CREATED).body(diaryEntryResponse);
     }
-
+    @PutMapping("/update/{id}")
+    public ResponseEntity<DiaryEntryResponse> updateDiaryEntry(
+            @PathVariable Long id,
+            @RequestBody UpdateDiaryEntryRequest request,
+            Authentication authentication) {
+        String email = authentication.getName();
+        DiaryEntryResponse response = diaryService.updateDiaryEntry(id, request, email);
+        return ResponseEntity.ok(response);
+    }
 }
