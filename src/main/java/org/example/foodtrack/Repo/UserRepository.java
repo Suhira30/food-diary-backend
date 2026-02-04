e64f2798abdac1a3b79d918b49306ff270800eb7
Suhira30 <suhirabalarajan@gmail.com>
2026-02-04 22:26:43 +0530

    feat: UserRepository.java : countRestaurantsCreatedByUser created

--- src/main/java/org/example/foodtrack/Repo/UserRepository.java
+++ src/main/java/org/example/foodtrack/Repo/UserRepository.java
@@ -2,6 +2,8 @@ package org.example.foodtrack.Repo;
 
 import org.example.foodtrack.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-
+    @Query("SELECT COUNT(r) FROM Restaurant r WHERE r.createdBy.id = :userId")
+    Integer countRestaurantsCreatedByUser(@Param("userId") Long userId);
 }
