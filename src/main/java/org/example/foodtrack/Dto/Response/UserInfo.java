759c44e188bcb6436b82c88ba0c282714043ce4a
Suhira30 <suhirabalarajan@gmail.com>
2026-02-05 23:25:06 +0530

    chore: getter setter added

--- src/main/java/org/example/foodtrack/Dto/Response/UserInfo.java
+++ src/main/java/org/example/foodtrack/Dto/Response/UserInfo.java
@@ -1,9 +1,13 @@
 package org.example.foodtrack.Dto.Response;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class UserInfo {
     private Long id;
     private String name;
