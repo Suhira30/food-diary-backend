7edebad2f2286151b410377541e50425a6d32508
Suhira30 <suhirabalarajan@gmail.com>
2026-02-07 00:05:22 +0530

    fix: getter added

--- src/main/java/org/example/foodtrack/Dto/Response/TimelineStats.java
+++ src/main/java/org/example/foodtrack/Dto/Response/TimelineStats.java
@@ -1,11 +1,13 @@
 package org.example.foodtrack.Dto.Response;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
+@Getter
 public class TimelineStats {
     private Integer totalEntries;
     private Integer entriesThisMonth;
