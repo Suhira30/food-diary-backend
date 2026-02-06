e1a78028fb76241b6ff7b28b20b49c4fb7ecd7b9
Suhira30 <suhirabalarajan@gmail.com>
2026-02-07 00:03:33 +0530

    fix: getter setter added

--- src/main/java/org/example/foodtrack/Dto/Response/UserTimelineResponse.java
+++ src/main/java/org/example/foodtrack/Dto/Response/UserTimelineResponse.java
@@ -1,12 +1,16 @@
 package org.example.foodtrack.Dto.Response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class UserTimelineResponse {
     private UserProfileResponse profile;
     private List<DiaryEntryResponse> timeline;
