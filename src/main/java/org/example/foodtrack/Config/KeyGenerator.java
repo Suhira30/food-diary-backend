dc88c64f44d2bbd5e9317b2b129f7002dffac162
Suhira30 <suhirabalarajan@gmail.com>
2026-02-10 23:08:20 +0530

    style: commented lines removed

--- src/main/java/org/example/foodtrack/Config/KeyGenerator.java
+++ src/main/java/org/example/foodtrack/Config/KeyGenerator.java
@@ -8,7 +8,6 @@ import java.util.Base64;
 
 public class KeyGenerator {
     public static void main(String[] args) {
-        // Generate a secure key
         SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
         System.out.println("Secure JWT Secret Key: " + base64Key);
