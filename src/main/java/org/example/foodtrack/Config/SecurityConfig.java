f0760ff1962c7abaa269fe2eb64c002d46d4a232
Suhira30 <suhirabalarajan@gmail.com>
2026-02-19 23:10:27 +0530

    chore :unused throw removed

--- src/main/java/org/example/foodtrack/Config/SecurityConfig.java
+++ src/main/java/org/example/foodtrack/Config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
@@ -43,7 +43,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
         return config.getAuthenticationManager();
     }
 }
